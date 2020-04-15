7becbe3ccc421a4eed92a58e11e41cbdebe5e9d0
Aaron Zheng Visual Studio <zhengheng36@gmail.com>
2020-04-15 12:48:51 -0400

    Updates

---
+++ precache-manifest.90d8bee84e2a872a33907b3a0bc49234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbfae2d3d7bcd745a94e34d82c4951d",
+    "url": "/Test_DeployReactToGitHub/index.html"
+  },
+  {
+    "revision": "026d100b1e33ec54b544",
+    "url": "/Test_DeployReactToGitHub/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "134def81b153d3636e4f",
+    "url": "/Test_DeployReactToGitHub/static/js/2.3c16eec8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Test_DeployReactToGitHub/static/js/2.3c16eec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026d100b1e33ec54b544",
+    "url": "/Test_DeployReactToGitHub/static/js/main.7e700cae.chunk.js"
+  },
+  {
+    "revision": "8ae16e452d02fde3d726",
+    "url": "/Test_DeployReactToGitHub/static/js/runtime-main.32111418.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Test_DeployReactToGitHub/static/media/logo.5d5d9eef.svg"
+  }
+]);
